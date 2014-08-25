8de7ecbf02f4eade92a7c771b274003e67a5b681
App Academy Student <student@appacademy.io>
2014-08-25 15:26:35 -0400

    refactor specs

--- spec/drawing_tool_spec.rb
+++ spec/drawing_tool_spec.rb
@@ -3,13 +3,22 @@ require 'drawing_tool'
 
 describe Canvas do
   subject(:canvas) {Canvas.new(20, 4)}
+  let(:blank_canvas) do 
+    [
+      "----------------------", 
+      "|                    |",
+      "|                    |",
+      "|                    |",
+      "|                    |",
+      "----------------------"
+    ]
+  end
+  
   describe "#initialize" do
     it "initializes a canvas with the correct dimensions" do
-      expect(canvas.render).to eq(
-        ["-" * 22] + (["|#{" " * 20}|"] * 4) + ["-" * 22]
-      )
+      expect(canvas.render).to eq(blank_canvas)
     end
-    
+  
     it "raises error when invalid dimensions are given" do
       expect {Canvas.new(0, 8)}.to raise_error("invalid canvas size")
       expect {Canvas.new(1, -3)}.to raise_error("invalid canvas size")
