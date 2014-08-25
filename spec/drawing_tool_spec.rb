b13a67195e2e0a280a94f50879054b27c82abd4f
App Academy Student <student@appacademy.io>
2014-08-25 15:09:11 -0400

    add spec to check #initialize raises invalid dimensions error

--- spec/drawing_tool_spec.rb
+++ spec/drawing_tool_spec.rb
@@ -9,5 +9,10 @@ describe Canvas do
         ["-" * 22] + (["|#{" " * 20}|"] * 4) + ["-" * 22]
       )
     end
+    
+    it "raises error when invalid dimensions are given" do
+      expect {Canvas.new(0, 8)}.to raise_error("invalid canvas size")
+      expect {Canvas.new(1, -3)}.to raise_error("invalid canvas size")
+    end
   end
 end
