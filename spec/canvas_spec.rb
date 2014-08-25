d1ad4dd30cebedb763e53e251739b8367b727b72
App Academy Student <student@appacademy.io>
2014-08-25 17:40:10 -0400

    refactor specs

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -3,20 +3,17 @@ require 'canvas'
 
 describe Canvas do
   subject(:canvas) {Canvas.new(20, 4)}
-  let(:blank_canvas) do 
-    [
+  
+  describe "#initialize" do
+    it "initializes a canvas with the correct dimensions" do
+      expect(canvas.render).to eq([
       "----------------------", 
       "|                    |",
       "|                    |",
       "|                    |",
       "|                    |",
       "----------------------"
-    ]
-  end
-  
-  describe "#initialize" do
-    it "initializes a canvas with the correct dimensions" do
-      expect(canvas.render).to eq(blank_canvas)
+    ])
     end
   
     it "raises error when invalid dimensions are given" do
@@ -28,17 +25,25 @@ describe Canvas do
   describe "#draw_line" do
     it "draws a horizontal line" do
       # the user's coordinates will begin at origin (1, 1), but our code will refer to the origin as (0, 0)
-      rendered_canvas = blank_canvas
-      rendered_canvas[2] = "|#{"x" * 6}#{" " * 14}|"
-      expect(canvas.draw_line(0, 1, 5, 1).render).to eq(rendered_canvas)
+      expect(canvas.draw_line(0, 1, 5, 1).render).to eq([
+        "----------------------", 
+        "|                    |",
+        "|xxxxxx              |",
+        "|                    |",
+        "|                    |",
+        "----------------------"
+      ])
     end
     
     it "draws a vertical line" do
-      rendered_canvas = blank_canvas
-      new_str = "|#{" " * 5}x#{" " * 14}|"
-      rendered_canvas[3] = new_str
-      rendered_canvas[4] = new_str
-      expect(canvas.draw_line(5, 2, 5, 3).render).to eq(rendered_canvas)
+      expect(canvas.draw_line(5, 2, 5, 3).render).to eq([
+        "----------------------", 
+        "|                    |",
+        "|                    |",
+        "|     x              |",
+        "|     x              |",
+        "----------------------"
+      ])
     end
     
     it "raises error if line is drawn off the canvas" do
