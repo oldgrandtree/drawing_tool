fc737995075bb939a2572d7c36ebb5c6e17bfe16
App Academy Student <student@appacademy.io>
2014-08-25 14:58:05 -0400

    implement Canvas#initialize spec

--- spec/drawing_tool_spec.rb
+++ spec/drawing_tool_spec.rb
@@ -2,5 +2,12 @@ require 'rspec'
 require 'drawing_tool'
 
 describe Canvas do
-  
+  subject(:canvas) {Canvas.new(20, 4)}
+  describe "#initialize" do
+    it "initializes a canvas with the correct dimensions" do
+      expect(canvas.render).to eq(
+        ["-" * 22] + (["|#{" " * 20}|"] * 4) + ["-" * 22]
+      )
+    end
+  end
 end
