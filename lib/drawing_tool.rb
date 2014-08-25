011e65cc6baf18f9debb8587d231f842b8a32e4c
App Academy Student <student@appacademy.io>
2014-08-25 15:03:41 -0400

    implement Canvas#render

--- lib/drawing_tool.rb
+++ lib/drawing_tool.rb
@@ -1,5 +1,14 @@
 class Canvas
-  def initialize(x, y)
+  def initialize(x, y)  
     @canvas = Array.new(y) { Array.new(x) }
+  end
+  
+  def render
+    output = []
+    output << ("-" * (@canvas.first.length + 2))
+    @canvas.each do |line| 
+      output << "|#{line.map {|square| square || " "}.join}|"
+    end
+    output << ("-" * (@canvas.first.length + 2))
   end
 end
