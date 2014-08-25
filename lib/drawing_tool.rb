a1c28d3e1cea1f989ee1dcf989b67aa2c04af11e
App Academy Student <student@appacademy.io>
2014-08-25 17:01:10 -0400

    implement #fill

--- lib/drawing_tool.rb
+++ lib/drawing_tool.rb
@@ -1,9 +1,26 @@
+require 'debugger'
+
 class Canvas
+  DIRS = [[1, -1], [1, 0], [1, 1], [0, -1], [0, 1], [-1, -1], [-1, 0], [-1, 1]]
+  
   def initialize(x, y)  
     # this condition ensures @canvas.first is an array, avoiding an error in #valid_pos? if y was equal to 0
     raise "Invalid canvas size." unless [x,y].all? {|coord| coord > 0}
     
     @canvas = Array.new(y) { Array.new(x) }
+  end
+  
+  def render
+    output = []
+    output << ("-" * (@canvas.first.length + 2))
+    @canvas.each do |line| 
+      output << "|#{line.map {|square| square || " "}.join}|"
+    end
+    output << ("-" * (@canvas.first.length + 2))
+  end
+  
+  def display
+    puts render
   end
   
   def [](*pos)
@@ -66,16 +83,25 @@ class Canvas
     self
   end
   
-  def render
-    output = []
-    output << ("-" * (@canvas.first.length + 2))
-    @canvas.each do |line| 
-      output << "|#{line.map {|square| square || " "}.join}|"
-    end
-    output << ("-" * (@canvas.first.length + 2))
-  end
-  
-  def display
-    puts render
+  def fill(x, y, color)
+    #will raise error if (x, y) is off the board
+    target_color = self[x, y]
+    
+    spaces_queue = [[x, y]]
+    
+    until spaces_queue.empty?
+      current_space = spaces_queue.pop
+      
+      # if the current space in the queue has the color we are filling, fill it in with the new color and add its neighbors to the queue
+      if valid_pos?(*current_space) && self[*current_space] == target_color
+        self[*current_space] = color
+        spaces_queue.concat(
+          DIRS.map {|dx, dy| [current_space[0] + dx, current_space[1] + dy]}
+        )
+      end
+    end 
+    
+    self
   end
 end
+
